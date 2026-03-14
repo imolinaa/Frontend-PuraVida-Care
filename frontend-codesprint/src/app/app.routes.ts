4886c613e49a5cb9f328e0dd9366870e394aaf0e
Stephabarca <stephabarca29@gmail.com>
2026-03-13 19:49:50 -0600

    feat: implementar login con Google y estilos de botón

--- frontend-codesprint/src/app/app.routes.ts
+++ frontend-codesprint/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'auth-success', component: AuthCallbackComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
